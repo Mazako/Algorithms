c52dec1fc5f482c07eab56a2da495a72798709c0
Michał Maziarz <michal.maziarz12@gmail.com>
2022-04-08 18:41:01 +0200

    Bubble sort implementation

---
+++ src/BubbleSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+
+public interface BubbleSort {
+
+    static void sort(double[] tab) {
+        for (int i = 0; i < tab.length - 1; i++) {
+            for (int j = 0; j < tab.length - 1 - i; j++) {
+                if (tab[j] > tab[j+1]) {
+                    swap(tab, j, j + 1);
+                }
+            }
+        }
+    }
+
+    static void swap(double[] tab, int a, int b) {
+        double tmp = tab[a];
+        tab[a] = tab[b];
+        tab[b] = tmp;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Test {
+    public static void main(String[] args) {
+        double[] tab = {3.0,2.0,1.0};
+        BubbleSort.sort(tab);
+        System.out.println(Arrays.toString(tab));
+    }
+}
