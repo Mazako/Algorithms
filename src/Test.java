4946111abfbafc97b7207baec4950b83583a1157
Michał Maziarz <michal.maziarz12@gmail.com>
2022-04-10 17:47:27 +0200

    Random array generator implementation

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,24 @@
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Random;
 
 public class Test {
+    private static final Random rng = new Random();
+
     public static void main(String[] args) {
-        double[] tab = {3.0,2.0,1.0};
+        double[] tab = createRandomArr(10);
         BubbleSort.sort(tab);
+        System.out.println(Arrays.toString(tab));
     }
+
+    private static double[] createRandomArr(int len) {
+        double[] tab = new double[len];
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = rng.nextInt(1000) + 1;
+        }
+
+        return tab;
+    }
+
 }
